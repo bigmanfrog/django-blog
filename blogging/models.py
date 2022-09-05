44ccfaf54ea20387d7c71bff3ad654cf0b0a4171
Thanh Nguyen <bigmanfrog@hotmail.com>
2022-09-05 09:02:03 -0700

    add blogging model and add to admin

--- blogging/models.py
+++ blogging/models.py
@@ -24,3 +24,13 @@ class Post(models.Model):
     def __str__(self) -> str:
         return self.title
 
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
