049af5706d6f60b2fcc9c0bf4e7eab2b39bbe791
Thanh Nguyen <bigmanfrog@hotmail.com>
2022-09-06 11:44:36 -0700

    complete detail template

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import stub_view
+from blogging.views import detail_view, list_view
 
 urlpatterns = [ 
-    path('', stub_view, name='blog_index'),
-    
+    path('', list_view, name='blog_index'),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
 
 # Create your views here.
-def blank_view(request):
-    context = 'blank' #{'polls': Poll.objects.all()}
-    return render(request, 'blogging/empty.html', context)
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type = 'text/html')
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context) #using a shortcut here instead of the 4 lines above
+
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
